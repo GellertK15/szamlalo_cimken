ba3e30bc42cebc4503ce7b66f6c6e785ad1b4cc2
GellertK15 <kiraly.gellert@gmail.com>
2025-12-05 11:07:52 +0100

    01_számlálo_a_cimken

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    document.title = `Ennyi kattintás: ${count}`
+  }, [count])
+
   return (
     <div className="container">
       <button
         className="kattintas"
-        title={`Kattintások száma: ${count}`}
+        title={`Kattintások száma: ${count}`} 
         onClick={() => setCount(count + 1)}
       >
         {count}
